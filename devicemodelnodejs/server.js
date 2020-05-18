a5b54d88fcfe274c668473be1befb79f3945252b
sudiptamukherjeeibm <sudipta.mukherjee@in.ibm.com>
2020-05-18 11:25:49 +0000

    deleted Test2

--- devicemodelnodejs/server.js
+++ devicemodelnodejs/server.js
@@ -53,50 +53,6 @@ var requst=	https.request(options, function (resp) {
 });
 
 
-app.get('/DeviceTest2', function (req, res) {
-	//	console.log(req.body);
-		console.log ("Hi below is request body of dev2");
-	var options = {
-		hostname: 'ibmprod.eu10.cp.iot.sap',
-		port: null,
-		//path: '/iot/processing/api/v1/tenant/2/measures/capabilities/'+capacityId+'?filter=(sensorId%20eq%20%27'+sensorId+'%27)&skip=0&top=100', 
-		path: '/iot/processing/api/v1/tenant/2/measures/capabilities/a0a616fa-9c58-466d-835e-18e5b8f6e5b5?filter=(sensorId%20eq%20%2752f54ec1-8df2-4e2a-bf11-5624a6b319fe%27)&skip=0&top=100', 
-		
-		//path: '/ibmprod/iot/core/api/v1/tenant/2/devices/cbe4ee84-048c-4b78-9e56-2565782f0099/measures?skip=0&top=100', 
-	//	https://ibmprod.eu10.cp.iot.sap/ibmprod/iot/core/api/v1/tenant/2/devices/cbe4ee84-048c-4b78-9e56-2565782f0099/measures?skip=0&top=100
-		method: 'GET',
-		
-		headers: {
-			   "cache-control": "no-cache",
-           "authorization": "Basic c3VkaXB0YW06SW5pdDEyMzQ="
-		}
-	};
-
-var requst=	https.request(options, function (resp) {
-		var body = '';
-		 var chunks = [];
-
-		resp.on('data', function (chunk) {
-			body += chunk;
-		  chunks.push(chunk);
-			
-		});
-		
-		resp.on('end', function () {
-			var dat = body;
-			 var bod = Buffer.concat(chunks);
-          //  console.log(bod.toString());
-		//	console.log ("Hi");
-		//	console.log(dat);
-			
-		res.type("application/json").status(200).send(dat);
-		});
-
-		//console.log("Server listening on port %d", port);
-	});
-    requst.end();
-
-});
 
 /*http.createServer(function (req, res) {
   res.writeHead(200, {"Content-Type": "text/plain"});
