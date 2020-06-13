8123ff065dff307106ad2346037fcc609346488e
Vinita-KumariIT <vkumarij@in.ibm.com>
2020-06-13 10:07:57 +0000

    GetSensorType,PostSensorType

--- devicemodelnodejs/server.js
+++ devicemodelnodejs/server.js
@@ -11,9 +11,146 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: false
 }));
+
+app.post('/GetSensorTypeFromMES', function (req, res) {
+	
+var HostName= req.body.HostName;
+var TenantId= req.body.TenantId;
+var Path= '/ibmprod/iot/core/api/v1/tenant/'+TenantId+'/sensorTypes';
+var options = {
+  method: 'GET',
+  hostname: HostName,
+  path: Path,
+  headers: {
+    "authorization": "Basic c3VkaXB0YW06SW5pdDEyMzQ=",
+	"content-type": "application/json"
+  }
+};
+var req = https.request(options, function (resp) {
+  var chunks = [];
+  resp.on('data', function (chunk) {
+    chunks.push(chunk);
+  });
+  resp.on("end", function (chunk) {
+    var body = Buffer.concat(chunks);
+    console.log(body.toString());
+	res.type("application/json").status(202).send(body);
+  });
+				  
+  resp.on("error", function (error) {
+    console.error(error);
+  });
+});
+req.end();
+});
+
+app.post('/CreateSensorTypeMES', function (req, res) {
+	//	console.log(req.body);
+var HostName= req.body.HostName;
+var TenantId= req.body.TenantId;	
+var alternateIdSensor = req.body.alternateId;
+var SensorName = req.body.name;
+var sensorId = req.body.id;
+var sensorType = req.body.type;
+//var UOM = req.body.unitOfMeasure;
+	console.log ("Hi below is request body for POST");
+	//console.log(fs.readFileSync(CERTIFICATE_FILE));
+	var PostMsg=
+				{
+				"alternateId":alternateIdSensor,
+					"capabilities":[
+						{
+							"id":sensorId,
+							"type":sensorType
+						}
+					],
+				"name":SensorName
+				};
+	
+			
+	var options = {
+		 method: 'POST',
+		 hostname: HostName,
+		 port: null,
+		 path: '/ibmprod/iot/core/api/v1/tenant/'+TenantId+'/sensorTypes',
+		 //cert: fs.readFileSync(CERTIFICATE_FILE_PEM),
+		 //key: fs.readFileSync(CERTIFICATE_FILE_PEM),
+         //passphrase: 'yTRWxuva3gR6HIbIhRzqe1Orfa8yulyYJy3x',
+        // "authorization": "Basic c3VkaXB0YW06SW5pdDEyMzQ=",
+		 headers: {
+		"accept": "*/*",
+    	"content-type": "application/json",
+    	"cache-control": "no-cache",
+    	"authorization": "Basic c3VkaXB0YW06SW5pdDEyMzQ="
+		}
+		/*agentOptions: {
+        pfx: fs.readFileSync(CERTIFICATE_FILE_P12),
+        passphrase: 'tHRsLVoPVeylHwRxWzOvVCxqAt1Pt8koIFS9'
+    	}*/
+	};
+	var dataEncoded=JSON.stringify(PostMsg);
+
+var requst=	https.request(options, function (resp) {
+		var body = '';
+		 var chunks = [];
+
+		resp.on('data', function (chunk) {
+			body += chunk;
+		  //chunks.push(chunk);
+			
+		});
+		
+		resp.on('end', function () {
+			var dat = body;
+			//var bod = Buffer.concat(chunks);
+
+		res.type("application/json").status(202).send(dat);
+		});
+
+	});
+	
+	//requst.write(postData);
+	requst.write(dataEncoded);
+    requst.end();
+
+});
+
+app.post('/GetCapabilitiesFromMES', function (req, res) {
+	
+var HostName= req.body.HostName;
+var TenantId= req.body.TenantId;
+var Path= '/ibmprod/iot/core/api/v1/tenant/'+TenantId+'/capabilities';
+var options = {
+  method: 'GET',
+  hostname: HostName,
+  path: Path,
+  headers: {
+    "authorization": "Basic c3VkaXB0YW06SW5pdDEyMzQ=",
+	"content-type": "application/json"
+  }
+};
+var req = https.request(options, function (resp) {
+  var chunks = [];
+  resp.on('data', function (chunk) {
+    chunks.push(chunk);
+  });
+  resp.on("end", function (chunk) {
+    var body = Buffer.concat(chunks);
+    console.log(body.toString());
+	res.type("application/json").status(202).send(body);
+  });
+				  
+  resp.on("error", function (error) {
+    console.error(error);
+  });
+});
+req.end();
+});
+
 app.post('/CreateCapabilityMES', function (req, res) {
 	//	console.log(req.body);
-	
+var HostName= req.body.HostName;
+var TenantId= req.body.TenantId;	
 var alternateIdCap = req.body.alternateId;
 var CapabilityName = req.body.name;
 var PropName = req.body.propertyname;
@@ -43,9 +180,9 @@ var UOM = req.body.unitOfMeasure;
 	
 	var options = {
 		 method: 'POST',
-		 hostname: 'ibmprod.eu10.cp.iot.sap',
+		 hostname: HostName,
 		 port: null,
-		 path: '/ibmprod/iot/core/api/v1/tenant/2/capabilities',
+		 path: '/ibmprod/iot/core/api/v1/tenant/'+TenantId+'/capabilities',
 		 //cert: fs.readFileSync(CERTIFICATE_FILE_PEM),
 		 //key: fs.readFileSync(CERTIFICATE_FILE_PEM),
          //passphrase: 'yTRWxuva3gR6HIbIhRzqe1Orfa8yulyYJy3x',
